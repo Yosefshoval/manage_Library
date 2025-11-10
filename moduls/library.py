c167844cfbd8bcce3a526363f29426b745776f32
Eliezer Yakovson <eliezeryakovson@gmail.com>
2025-11-10 16:45:56 +0200

    Adding try and except to FileHandler and Adding app search_book to class Library

--- moduls/library.py
+++ moduls/library.py
@@ -14,18 +14,24 @@ class Library:
     
 
     def find_user_and_book(self, user_id, book_isbn):
-        user = None
-        for current_user in self.users_list:
-            if current_user.id == user_id:
-                user = current_user
-                break
-        
-        book = None
-        for currebt_book in self.books_list:
-            if currebt_book.isbn == book_isbn:
-                book = currebt_book
-                break
+        valid_user = map(lambda u: u.id == user_id, self.users_list)
+        user = next(valid_user, None)
+        valid_book = map(lambda b: b.isbn == book_isbn, self.books_list)
+        book = next(valid_book, None)
         return book, user
+
+        
+        # for current_user in self.users_list:
+        #     if current_user.id == user_id:
+        #         user = current_user
+                # break
+        
+        # book = None
+        # for currebt_book in self.books_list:
+        #     if currebt_book.isbn == book_isbn:
+        #         book = currebt_book
+        #         break
+        
 
 
     def borrow_book(self, user_id, book_isbn):
@@ -42,8 +48,14 @@ class Library:
         return True
 
 
-    def search_book(self, title=None, auther=None):
-        pass
+    def search_book(self, title, auther):
+        results = []
+        for book in self.books_list:
+            results.extend(book for book in self.books_list if book.title.lower() == title.lower() or book.auther.lower() == auther.lower())
+        if not results:
+            return None   
+        return results
+        
 
 
     def reset_library(self, users, books):
--- service/file_handling.py
+++ service/file_handling.py
@@ -4,19 +4,25 @@ class FileHandler:
     
     @staticmethod
     def update_file(data : list, filename):
-        
-        diict_data = list(map(lambda item : item.__dict__, data))
+        try:
+            diict_data = list(map(lambda item : item.__dict__, data))
+   
+            json_data = json.dumps(diict_data, indent=3)
 
-        json_data = json.dumps(diict_data, indent=3)
-        
-        with open(filename, 'w') as file:
-            file.write(json_data)
+            with open(filename, 'w') as file:
+                file.write(json_data)
+        except Exception as e:
+            print(f'Error updating file: {e}')
 
     @staticmethod
     def read_file(data_type, object):
-        list_class = []
-        with open(f"../data/{data_type}.json", 'r') as file:
-            dict_file = json.load(file)
-            for item in dict_file:
-                list_class.append(object(**item))
-        return list_class
+        try:
+            list_class = []
+            with open(f"../data/{data_type}.json", 'r') as file:
+                dict_file = json.load(file)
+                for item in dict_file:
+                    list_class.append(object(**item))
+            return list_class
+        except Exception as e:
+            print(f'Error reading file: {e}')
+        
